0d60c9fd655f6f3b5ca5888e00c08e9aa2962ef2
Qvelent <48782273+Qvelent@users.noreply.github.com>
2021-10-27 23:32:10 +0300

    Add inpitSys, Eny Wave and Bourds

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrafab;
+    [SerializeField] float moveSpeed = 5f;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrafab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach(Transform child in pathPrafab)
+        {
+            waypoints.Add(child);
+        }
+        return waypoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+}

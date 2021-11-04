7f47525e0d946ed4846cb819fd80f6660fd91616
Qvelent <48782273+Qvelent@users.noreply.github.com>
2021-11-05 00:23:48 +0300

    Add shooting

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileLifeTime = 5f;
+    [SerializeField] float firingRate = 0.2f;
+
+    public bool isFiring;
+
+    Coroutine firingCoroutine;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if(isFiring && firingCoroutine == null)
+        {
+           firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        else if(!isFiring && firingCoroutine != null)
+        {
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
+        }
+        
+    }
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject instance = Instantiate(projectilePrefab, 
+                                              transform.position, 
+                                              Quaternion.identity);
+
+            Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+            if(rb != null)
+            {
+                rb.velocity = transform.up * projectileSpeed;
+            }                                  
+
+            Destroy(instance, projectileLifeTime);
+            yield return new WaitForSeconds(firingRate);                               
+        }
+    }
+}
